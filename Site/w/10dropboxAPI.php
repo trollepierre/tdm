8810b51e24d8444fff56deb78bd05b36f74f536c
trollepierre <trolle.pierre@gmail.com>
2014-08-22 11:26:05 +0200

    Update 10dropboxAPI.php
    nouveau authCode Dropbox

--- Site/w/10dropboxAPI.php
+++ Site/w/10dropboxAPI.php
@@ -20,7 +20,7 @@ $authorizeUrl = $webAuth->start();
 echo "1. Go to: " . $authorizeUrl . "\n";
 echo "2. Click \"Allow\" (you might have to log in first).\n";
 echo "3. Copy the authorization code.\n";
-$authCode = "c040st4Y9SsAAAAAAAAqygKXQtSfxbCr11f22Od875g";
+$authCode = "c040st4Y9SsAAAAAAAAqyz5EnsU0Afi-ERVYRgdvWx4";
 
 list($accessToken, $dropboxUserId) = $webAuth->finish($authCode);
 print "Access Token: " . $accessToken . "\n";
