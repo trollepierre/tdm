29fa49488b98ccfa783a6b1c8bf700391b6bc709
trollepierre <trolle.pierre@gmail.com>
2014-08-27 01:48:56 +0200

    Delete 00pull.php
    Ce fichier ne sert à rien.
    (Voici ce qu'est un pull request. A toi de l'accepter, tkt je sais ce que je fais. Tu acceptes de merger.)

--- Site/w/00pull.php
+++
@@ -1,7 +0,0 @@
-<?php `git pull`;
-$filename="PullGitHasWorked.txt"
-if (touch($filename)) {
-    echo $filename . ' modification time has been changed to present time';
-} else {
-    echo 'Sorry, could not change modification time of ' . $filename;
-}
