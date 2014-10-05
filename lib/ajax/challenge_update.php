ab360d6de9eba89f11d8be4dd385b5cd489fe2e8
trollepierre <trolle.pierre@gmail.com>
2014-10-06 01:47:42 +0200

    Challenge Trad

--- lib/ajax/challenge_update.php
+++ lib/ajax/challenge_update.php
@@ -24,9 +24,9 @@ while ($donnees = $reponse->fetch()){
     }
 $reponse->closeCursor(); // Termine le traitement de la requête
 
-$req = $bdd->prepare('INSERT INTO challenge(challenge_uid,name, img_link) VALUES(:challengeUid, :name, :img0)');
+$req = $bdd->prepare('INSERT INTO challenge(challenge_uid, nom, name, img_link) VALUES(:challengeUid, :nom, :name, :img0)');
 $reqG = $bdd->prepare('INSERT INTO challenge_galerie(challenge_uid, img_link) VALUES(:challengeUid, :urlGallery)');
-$reqI = $bdd->prepare('INSERT INTO challenge_contenu(challenge_uid, position, titre, soustitre, paragraphe, img_link) VALUES(:challengeUid, :position, :titre, :soustitre , :paragraphe, :img)');
+$reqI = $bdd->prepare('INSERT INTO challenge_contenu(challenge_uid, position, titre, soustitre, paragraphe, title, subtitle, paragraph, img_link) VALUES(:challengeUid, :position, :titre, :soustitre , :paragraphe, :title, :subtitle , :paragraph, :img)');
 
 /*
 * récupère les img et gallery 
@@ -36,10 +36,9 @@ include("lib/dropboxAPI.php");
 $myCustomClient = new dbx\Client($accessToken, $clientIdentifier);
 
 
+//Francais
 $basePath="/Chargements appareil photo/ChallengeTdm/".$id."";
-$query = "ext.php";
-$file="";
-//recup des files
+$query = "r.php";
 $returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
 foreach ($returnSearchFileName as $idFake => $texte) {
   foreach ($texte as $key => $value) {
@@ -48,13 +47,25 @@ foreach ($returnSearchFileName as $idFake => $texte) {
     }
   }
 }
-// echo $file;
 $data = file_get_contents($file); 
 $convert = explode("*", $data); //create array separate by *
-$name = $convert[0];
-$query = "jpg";
+$nom = $convert[0];
 
-//recup des files
+//English
+$query = "n.php";
+$returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
+foreach ($returnSearchFileName as $idFake => $texte) {
+  foreach ($texte as $key => $value) {
+    if($key=='path'){
+      $file=$myCustomClient->createTemporaryDirectLink($value)[0];
+    }
+  }
+}
+$data = file_get_contents($file); 
+$convert_en = explode("*", $data); //create array separate by *
+$name = $convert_en[0];
+
+$query = "jpg";
 $returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
 
 //on récup le path de chaque file récupéré
@@ -69,25 +80,29 @@ foreach ($returnSearchFileName as $idFake => $image) {
             $titre=$convert[3*$position-2];
             $soustitre=$convert[3*$position-1];
             $paragraphe=$convert[3*$position];
+            $title=$convert_en[3*$position-2];
+            $subtitle=$convert_en[3*$position-1];
+            $paragraph=$convert_en[3*$position];
             $img=$myCustomClient->createShareableLink($value);
             $img[strlen($img)-1]='1';
-            $reqI->execute(array( 'challengeUid' => $challengeUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'img' => $img));
-            // $url[$position]=$myCustomClient->createShareableLink($value);
+            $reqI->execute(array( 'challengeUid' => $challengeUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'title' => $title,'subtitle' => $subtitle, 'paragraph' => $paragraph, 'img' => $img));
            }else if(substr($value,-9,4)== "/img"){
             if(substr($value, -5)==0) {
               $img0 = $myCustomClient->createShareableLink($value);
               $img0[strlen($img0)-1]='1';
-              $req->execute(array( 'challengeUid' => $challengeUid, 'name' => $name, 'img0' => $img0));
+              $req->execute(array( 'challengeUid' => $challengeUid, 'nom' => $nom, 'name' => $name, 'img0' => $img0));
           }else{
             $position= intval(substr($value, -5,1));
             $titre=$convert[3*$position-2];
             $soustitre=$convert[3*$position-1];
             $paragraphe=$convert[3*$position];
+            $title=$convert_en[3*$position-2];
+            $subtitle=$convert_en[3*$position-1];
+            $paragraph=$convert_en[3*$position];
             $img=$myCustomClient->createShareableLink($value);
             $img[strlen($img)-1]='1';
-            $reqI->execute(array( 'challengeUid' => $challengeUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'img' => $img));
+            $reqI->execute(array( 'challengeUid' => $challengeUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'title' => $title,'subtitle' => $subtitle, 'paragraph' => $paragraph, 'img' => $img));
             // $url[$position]=$myCustomClient->createShareableLink($value);
-            
             }
           }
           else{
