62467ac039523ce8796de5e81c2cdea33092c0e1
trollepierre <trolle.pierre@gmail.com>
2014-10-08 10:21:30 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@ function verify(){
 }
 
 verify();
-echo '\nchallenge envoyé\n';
+echo "\nchallenge sent\n";
 /*
 from hashlib import sha256
 import hmac
@@ -17,20 +17,21 @@ function webhook(){
 
 	//1 recup de l'header et verifie si signature dropbox
 	$signature = $_GET['X-Dropbox-Signature'];
-	echo 'sign     \n';
+	echo "sign n";
 	echo $signature;
+	//comment vérifier la signature ? (non facultatif)
+
 	//2 recup du json
-	$json = $POST['json'];
-	echo $json;
-	$texte = $signature."\n<br/>\n".$json;
+	$json = $POST['data'];
+	$json2 = $POST['delta'];
+	$json3 = 	echo $json;
+	$texte = $signature."\n<br/>\n".$json."\n<br/>\n".$json2."\n<br/>\n".$json3;
 	file_put_contents('dblog.txt',$texte);
+
+	//3 repondre rapidement
+	//je sais pas
+
 	
-	/*	  for uid in json.loads(request.data)['delta']['users']:
-        # We need to respond quickly to the webhook request, so we do the
-        # actual work in a separate thread. For more robustness, it's a
-        # good idea to add the work to a reliable queue and process the queue
-        # in a worker process.
-        threading.Thread(target=process_user, args=(uid,)).start()*/
 }
 
 /*@app.route('/webhook', methods=['POST'])
@@ -39,6 +40,7 @@ def webhook():
 
     # Make sure this is a valid request from Dropbox
     signature = request.headers.get('X-Dropbox-Signature')
+$POST['json'];
     if signature != hmac.new(APP_SECRET, request.data, sha256).hexdigest():
         abort(403)
 
