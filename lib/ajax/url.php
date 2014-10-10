02c1f8e181ae413155209c9c9ed5da2bb8d3166c
trollepierre <trolle.pierre@gmail.com>
2014-10-11 00:16:08 +0200

    URL.php résiste à l'incomplétude

--- lib/ajax/url.php
+++ lib/ajax/url.php
@@ -16,11 +16,11 @@ use \Dropbox as dbx;
 $articleUid=$id;
 
 include("lib/creerBdd.php");
-$reponse = $bdd->query('SELECT article_uid FROM article WHERE article_uid='.$id.' ORDER BY id');
+$reponse = $bdd->query('SELECT article_uid FROM article WHERE article_uid='.$id);
 while ($donnees = $reponse->fetch()){
-    $bdd->exec('DELETE FROM article_galerie WHERE article_uid='.$id.'  ');
-    $bdd->exec('DELETE FROM article_contenu WHERE article_uid='.$id.'  ');
-    $bdd->exec('DELETE FROM article WHERE article_uid='.$id.'  ');
+    $bdd->exec('DELETE FROM article_galerie WHERE article_uid='.$id);
+    $bdd->exec('DELETE FROM article_contenu WHERE article_uid='.$id);
+    $bdd->exec('DELETE FROM article WHERE article_uid='.$id);
     }
 $reponse->closeCursor(); // Termine le traitement de la requête
 
@@ -41,6 +41,7 @@ $basePath="/Chargements appareil photo/ArticleTdm/".$id."";
 $dropbox_link=$myCustomClient->createShareableLink($basePath);
 
 //Francais
+$file="lib/fr.txt";
 $query = "r.php";
 $returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
 foreach ($returnSearchFileName as $idFake => $texte) {
@@ -55,6 +56,7 @@ $convert = explode("*", $data); //create array separate by *
 $nom = $convert[0];
 
 //English
+$file="lib/en.txt";
 $query = "n.php";
 $returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
 foreach ($returnSearchFileName as $idFake => $texte) {
@@ -75,17 +77,45 @@ $returnSearchFileName=$myCustomClient->searchFileNames($basePath, $query);
 // et on en fait une url publique;
 $urlGallery = array( );
 $url = array( );
+
+function convert($convert, $convert_en,$position){
+  if (count($convert)<=(1+3*$position)) {
+    $titre=$convert[3*$position-2];
+    $soustitre=$convert[3*$position-1];
+    $paragraphe=$convert[3*$position];
+    if (count($convert_en)<=(1+3*$position)) {
+      $title=$convert_en[3*$position-2];
+      $subtitle=$convert_en[3*$position-1];
+      $paragraph=$convert_en[3*$position];
+    }else{
+      $title=$titre;
+      $subtitle=$soustitre;
+      $paragraph=$paragraphe;
+    }
+  }else{
+    if (count($convert_en)<=(1+3*$position))  {
+      $title=$convert_en[3*$position-2];
+      $subtitle=$convert_en[3*$position-1];
+      $paragraph=$convert_en[3*$position];
+      $titre=$title;
+      $soustitre=$subtitle;
+      $paragraphe=$paragraph;
+    }else{
+      $title = "Article not written yet";
+      $titre = "Article pas encore rédigé";
+      $paragraph = "To make you wait, find the first pictures below";
+      $paragraphe = "Pour vous faire patienter, retrouver les photos de l'article ci-dessous";
+    }
+  }
+}
+
+
 foreach ($returnSearchFileName as $idFake => $image) {
   foreach ($image as $key => $value) {
         if($key=='path'){
           if(substr($value,-9,3)=='img'){
             $position= intval(substr($value, -6,2));
-            $titre=$convert[3*$position-2];
-            $soustitre=$convert[3*$position-1];
-            $paragraphe=$convert[3*$position];
-            $title=$convert_en[3*$position-2];
-            $subtitle=$convert_en[3*$position-1];
-            $paragraph=$convert_en[3*$position];
+            convert($convert,$convert_en,$position);
             $img=$myCustomClient->createShareableLink($value);
             $img[strlen($img)-1]='1';
             $reqI->execute(array( 'articleUid' => $articleUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'title' => $title,'subtitle' => $subtitle, 'paragraph' => $paragraph, 'img' => $img));
@@ -96,27 +126,47 @@ foreach ($returnSearchFileName as $idFake => $image) {
               $req->execute(array( 'articleUid' => $articleUid, 'nom' => $nom, 'name' => $name, 'img0' => $img0, 'dropbox_link' => $dropbox_link));
           }else{
             $position= intval(substr($value, -5,1));
-            $titre=$convert[3*$position-2];
-            $soustitre=$convert[3*$position-1];
-            $paragraphe=$convert[3*$position];
-            $title=$convert_en[3*$position-2];
-            $subtitle=$convert_en[3*$position-1];
-            $paragraph=$convert_en[3*$position];
+            convert($convert,$convert_en,$position);
             $img=$myCustomClient->createShareableLink($value);
             $img[strlen($img)-1]='1';
             $reqI->execute(array( 'articleUid' => $articleUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'title' => $title,'subtitle' => $subtitle, 'paragraph' => $paragraph, 'img' => $img));
-            // $url[$position]=$myCustomClient->createShareableLink($value);
             }
           }
           else{
              $urlGallery= $myCustomClient->createShareableLink($value);
            $urlGallery[strlen($urlGallery)-1]='1';
            $reqG->execute(array( 'articleUid' => $articleUid, 'urlGallery' => $urlGallery));
-           // $urlGallery[]=$myCustomClient->createShareableLink($value);          
           }
         }
     }
 }
+$reponse = $bdd->query('SELECT count(*) AS count FROM article WHERE article_uid='.$id);
+while ($donnees = $reponse->fetch()){
+    $isImg= htmlspecialchars($donnees['count']) ;
+    if ($isImg==0) {
+      $img0 = 'http://www.recontact.me/img/lost.jpg';
+      $req->execute(array( 'articleUid' => $articleUid, 'nom' => $nom, 'name' => $name, 'img0' => $img0, 'dropbox_link' => $dropbox_link));
+    }
+}
+$reponse->closeCursor();
+$reponse = $bdd->query('SELECT count(*) AS count FROM article_contenu WHERE article_uid='.$id);
+while ($donnees = $reponse->fetch()){
+    $isImgs= htmlspecialchars($donnees['count']) ;
+    $max=max(count($convert),count($convert_en));
+    if ($isImgs< $max) {
+      $reponse->closeCursor();
+      for ($position=1; $position < $max; $position++) { 
+        $reponse = $bdd->query('SELECT count(*) AS count FROM article_contenu WHERE article_uid='.$id.' AND position='.$position);
+        if (htmlspecialchars($donnees['count'])== 0) {
+          convert($convert,$convert_en,$position);
+          $img = 'http://www.recontact.me/img/lost.jpg';
+          $reqI->execute(array( 'articleUid' => $articleUid, 'position' => $position,'titre' => $titre,'soustitre' => $soustitre, 'paragraphe' => $paragraphe, 'title' => $title,'subtitle' => $subtitle, 'paragraph' => $paragraph, 'img' => $img));
+        }
+      }
+    }
+}
+$reponse->closeCursor();
+
 $req->closeCursor(); 
 $reqI->closeCursor(); 
 $reqG->closeCursor(); 
