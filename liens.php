ff4552a32155da4dc7b8c742c43d58531ddcf6de
trollepierre <trolle.pierre@gmail.com>
2014-10-17 02:28:31 +0200

    Commentaires via Facebook

--- liens.php
+++ liens.php
@@ -3,6 +3,14 @@
 ?>
 </head>
 <body class="home page page-id-4 page-template page-template-page-home-php custom-background">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=661854923873753&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <?php require("w/2lightbox.php");?>
 
  <!-- PAGE 1/2  ===========================================================================-->
@@ -26,6 +34,8 @@
 </div>
 
 <?php require("w/7comments.php");?>
+<div class="fb-comments" data-width="1500px" data-numposts="5" data-colorscheme="light"></div>
+
 <div class="espace"> . </div>
 <?php require("w/8footer.php");?>
 <?php require("w/9end.php");?>
--- merci.php
+++ merci.php
@@ -3,6 +3,15 @@
 ?>
 </head>
 <body class="home page page-id-4 page-template page-template-page-home-php custom-background">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=661854923873753&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php require("w/2lightbox.php");?>
 
  <!-- PAGE 1/2  ===========================================================================-->
@@ -11,6 +20,7 @@
 <div class="text-align">
 	<div id="gros"><?php echo THANK; ?></div>
 </div>
+<div class="fb-comments" data-width="1500px" data-numposts="5" data-colorscheme="light"></div>
 
 <?php require("w/8footer.php");?>
 <?php require("w/9end.php");?>
