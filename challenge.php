8d34dada7b02e63e53e9960dd0553b31dc380929
trollepierre <trolle.pierre@gmail.com>
2014-10-18 15:11:14 +0200

    Facebook sur article et challenge

--- challenge.php
+++ challenge.php
@@ -20,6 +20,15 @@
 </head>
 
 <body class="home page page-id-4 page-template page-template-page-home-php custom-background">
+       <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&appId=661854923873753&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php require("w/2lightbox.php");?>
 <?php require("w/4headerScroller.php");?>
 
@@ -106,6 +115,9 @@
                 ?>
             </ul>
         </div>
+    </div>
+     <div class="fbCenter">
+        <div class="fb-comments" data-href="https://www.facebook.com/recontact.me" data-width="600px" data-numposts="5" data-colorscheme="light"></div>
     </div>
     <div class="espace">. </div>
     <div id="caracteristiques"></div><!--INDISPENSABLE : WHY?-->
