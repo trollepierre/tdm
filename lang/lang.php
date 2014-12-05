60b41ef56fcac930ad68de6c067700de55ddbc12
trollepierre <trolle.pierre@gmail.com>
2014-12-05 10:48:53 -0400

    Update lang.php
    lang = fr.php

--- lang/lang.php
+++ lang/lang.php
@@ -2,21 +2,16 @@
  	if(isset($_COOKIE['lang'])) {
  	     $lang = $_COOKIE['lang'];
 	} else {
- 	     // si aucune langue n'est déclarée on tente de reconnaitre la langue par défaut du navigateur
+ 	     // si aucune langue n'est dÃ©clarÃ©e on tente de reconnaitre la langue par dÃ©faut du navigateur
 	     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2); 
  	}
-// $lang = 'en';
- 	 //script d'origine
- 	 if ($lang=='fr') {           // si la langue est 'fr' (français) on inclut le fichier fr-lang.php
- 	     include('lang/fr.php'); 
-	 } elseif ($lang=='en') {      // si la langue est 'en' (anglais) on inclut le fichier en-lang.php
- 	     include('lang/en.php'); 
-	 }  else {                       // si aucune langue n'est déclarée on inclut le fichier fr-lang.php par défaut
-		include('lang/en.php');
-	}
- 	 //fin du script d'origine
+
+	$lang=='fr'  ;       
+ 	include('lang/fr.php'); 
+	 
  	 
- 	 //définition de la durée du cookie (1 an)
+ 	 
+ 	 //dÃ©finition de la durÃ©e du cookie (1 an)
  	 $expire = 365*24*3600; 
  	 
 	//enregistrement du cookie au nom de lang
