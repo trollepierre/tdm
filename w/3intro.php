10bd4d97903038a9226b0c1db75e79bbca08aafc
trollepierre <trolle.pierre@gmail.com>
2014-12-06 11:36:15 -0400

    Update 3intro.php

--- w/3intro.php
+++ w/3intro.php
@@ -5,9 +5,9 @@
 <script type="text/javascript">
 compte_a_rebours();
 </script>
-        <a href="https://www.youtube.com/watch?v=HJlf-5a_DSw&list=PL-CUk3xAtkr4fjCMMOlLSILE24uaEZMq-" class="play mobile-media" target="_blank"></a>
+        <a href="https://www.youtube.com/watch?v=oIvdCv7iyG4&list=PL-CUk3xAtkr4b2StoJvThUCem7yYHt15M" class="play mobile-media" target="_blank"></a>
        // <a href="http://www.youtube.com/watch?v=11GYvfYjyV0&amp;feature=kp" class="play fancybox-media" target="_blank"></a>
-        <a href="https://www.youtube.com/watch?v=HJlf-5a_DSw&list=PL-CUk3xAtkr4fjCMMOlLSILE24uaEZMq-" class="play fancybox-media" target="_blank"></a>
+        <a href="https://www.youtube.com/watch?v=oIvdCv7iyG4&list=PL-CUk3xAtkr4b2StoJvThUCem7yYHt15M" class="play fancybox-media" target="_blank"></a>
         <div class="right">
             <p>
                 <?php echo RETRIEVE; ?>
