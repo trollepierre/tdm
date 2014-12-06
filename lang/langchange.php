f48c6798308186511f3237ff7b6a6d61c62276a5
trollepierre <trolle.pierre@gmail.com>
2014-12-06 13:15:37 -0400

    Update langchange.php

--- lang/langchange.php
+++ lang/langchange.php
@@ -1,18 +1,6 @@
 <?php		
-		
- if(isset($_COOKIE['lang'])) {
- 	     $lang = $_COOKIE['lang'];
-	 } else {
- 	     // si aucune langue n'est déclarée on tente de reconnaitre la langue par défaut du navigateur
-	     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2); 
- }		
- 	 if ($lang=='fr') {           
- 	     $lang = 'en';
-	 } elseif ($lang=='en') {     
+
  	     $lang = 'fr';
-	 }  else {                    
-		$lang = 'en';
-	}
 			//définition de la durée du cookie (1 an)
 			$expire = 365*24*3600; 
 		//	$expire = 10; 
