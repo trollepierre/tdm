e9ae06b1cb19f8eb9646486fc16df62251cbf386
trollepierre <trolle.pierre@gmail.com>
2015-03-15 13:31:21 +1100

    Nouvelle video

--- w/3intro.php
+++ w/3intro.php
@@ -5,9 +5,9 @@
 <script type="text/javascript">
 compte_a_rebours();
 </script>
-        <a href="https://www.youtube.com/watch?v=oIvdCv7iyG4&list=PL-CUk3xAtkr4b2StoJvThUCem7yYHt15M" class="play mobile-media" target="_blank"></a>
+        <a href="https://www.youtube.com/watch?v=_bU2aLrnKog" class="play mobile-media" target="_blank"></a>
        // <a href="http://www.youtube.com/watch?v=11GYvfYjyV0&amp;feature=kp" class="play fancybox-media" target="_blank"></a>
-        <a href="https://www.youtube.com/watch?v=oIvdCv7iyG4&list=PL-CUk3xAtkr4b2StoJvThUCem7yYHt15M" class="play fancybox-media" target="_blank"></a>
+        <a href="https://www.youtube.com/watch?v=_bU2aLrnKog" class="play fancybox-media" target="_blank"></a>
         <div class="right">
             <p>
                 <?php echo RETRIEVE; ?>
